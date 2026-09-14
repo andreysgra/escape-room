6e9ffffc9ecce59f952c9a270bc1dd43e7bd4eb5
Andrey Grachev <andreysgra@gmail.com>
2026-09-14 14:33:19 +0300

    Добавляет в приложение проверку авторизации пользователя.

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import store from './store';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {fetchQuests} from './store/quests/api-actions';
+import {fetchUserStatus} from './store/user/api-actions';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+store.dispatch(fetchUserStatus());
 store.dispatch(fetchQuests());
 
 root.render(
