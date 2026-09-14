e6ce404c991915c5b76f30f1922c31ffd2e0cbfa
Andrey Grachev <andreysgra@gmail.com>
2026-09-14 12:47:49 +0300

    Добавляет интерфейс работы с сервером.

---
+++ src/services/api/api.ts
@@ -0,0 +1,38 @@
+import axios, {AxiosError, AxiosInstance, HttpStatusCode, InternalAxiosRequestConfig} from 'axios';
+import {BASE_URL, REQUEST_TIMEOUT} from './const';
+import {getToken} from '../token';
+import {ResponseError} from './type';
+import {toast} from 'react-toastify';
+
+export const createApi = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: REQUEST_TIMEOUT
+  });
+
+  api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<ResponseError>) => {
+      toast.dismiss();
+
+      if (error.response?.status !== HttpStatusCode.Unauthorized) {
+        toast.error(error.response ? error.response.data.message : error.message);
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return api;
+};
--- src/services/api/const.ts
+++ src/services/api/const.ts
@@ -3,3 +3,7 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN'
 }
+
+export const BASE_URL = 'https://grading.design.htmlacademy.pro/v1/escape-room';
+
+export const REQUEST_TIMEOUT = 5000;
