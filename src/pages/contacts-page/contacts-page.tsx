89944630f0d453a2e2b689b4324a243f2031d618
Andrey Grachev <andreysgra@gmail.com>
2026-09-15 13:37:07 +0300

    Создаёт компонент с информацией контактов.

--- src/pages/contacts-page/contacts-page.tsx
+++ src/pages/contacts-page/contacts-page.tsx
@@ -1,3 +1,5 @@
+import ContactsList from '../../components/contacts-list/contacts-list';
+
 function ContactsPage() {
   return (
     <main className="page-content decorated-page">
@@ -24,39 +26,7 @@ function ContactsPage() {
           <h1 className="title title--size-m page-content__title">Контакты</h1>
         </div>
         <div className="contacts">
-          <dl className="contacts__list">
-            <div className="contacts__item">
-              <dt className="contacts__dt">Адрес</dt>
-              <dd className="contacts__dd">
-                <address className="contacts__address">
-                  Санкт-Петербург,
-                  <br/> Набережная реки Карповка, д 5П
-                </address>
-              </dd>
-            </div>
-            <div className="contacts__item">
-              <dt className="contacts__dt">Режим работы</dt>
-              <dd className="contacts__dd">
-                Ежедневно, с&nbsp;10:00 до&nbsp;22:00
-              </dd>
-            </div>
-            <div className="contacts__item">
-              <dt className="contacts__dt">Телефон</dt>
-              <dd className="contacts__dd">
-                <a className="link" href="tel:88003335599">
-                  8 (000) 111-11-11
-                </a>
-              </dd>
-            </div>
-            <div className="contacts__item">
-              <dt className="contacts__dt">E–mail</dt>
-              <dd className="contacts__dd">
-                <a className="link" href="mailto:info@escape-room.ru">
-                  info@escape-room.ru
-                </a>
-              </dd>
-            </div>
-          </dl>
+          <ContactsList />
           <div className="contacts__map">
             <div className="map">
               <div className="map__container"/>
