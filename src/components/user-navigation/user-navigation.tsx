66321feb474a8d9847dced405ff89e8e130e98f5
Andrey Grachev <andreysgra@gmail.com>
2026-09-16 18:22:11 +0300

    Реализует отображение e-mail пользователя.

--- src/components/user-navigation/user-navigation.tsx
+++ src/components/user-navigation/user-navigation.tsx
@@ -1,7 +1,7 @@
 import {Link, useLocation} from 'react-router-dom';
 import {AppRoute} from '../../const';
 import {useAppSelector} from '../../hooks/use-app-selector';
-import {getAuthorizationStatus} from '../../store/user/selectors';
+import {getAuthorizationStatus, getUser} from '../../store/user/selectors';
 import {AuthorizationStatus} from '../../services/api/const';
 import {useAppDispatch} from '../../hooks/use-app-dispatch';
 import {logoutUser} from '../../store/user/api-actions';
@@ -9,6 +9,7 @@ import {logoutUser} from '../../store/user/api-actions';
 function UserNavigation() {
   const isLoginPage = useLocation().pathname === AppRoute.Login as string;
   const isAuthorized = useAppSelector(getAuthorizationStatus) === AuthorizationStatus.Auth;
+  const user = useAppSelector(getUser);
 
   const dispatch = useAppDispatch();
 
@@ -21,9 +22,21 @@ function UserNavigation() {
   return (
     <div className="header__side-nav">
       {!isLoginPage && (isAuthorized ? (
-        <Link className="btn btn--accent header__side-item" to={AppRoute.Main} onClick={handleLogoutClick}>Выйти</Link>
+        <Link
+          className="btn btn--accent header__side-item"
+          title={user?.email}
+          to={AppRoute.Main}
+          onClick={handleLogoutClick}
+        >
+          Выйти
+        </Link>
       ) : (
-        <Link className="btn header__side-item header__login-btn" to={AppRoute.Login}>Вход</Link>
+        <Link
+          className="btn header__side-item header__login-btn"
+          to={AppRoute.Login}
+        >
+          Вход
+        </Link>
       ))}
       <a className="link header__side-item header__phone-link" href="tel:88003335599">8 (000) 111-11-11</a>
     </div>
